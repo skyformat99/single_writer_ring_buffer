00c49ede229750e08b94d968a163fa6b7c1fe910
Reid Paape <wrpaape@gmail.com>
2017-09-01 23:54:33 -0500

    under/over/even fill cases all stuffed into loop

--- src/single_writer_ring_buffer/test/test_single_writer_ring_buffer_insertion.cpp
+++ src/single_writer_ring_buffer/test/test_single_writer_ring_buffer_insertion.cpp
@@ -8,29 +8,29 @@
 
 
 
-
-TEST(insertion, single_thread_under_fill)
+TEST(insertion, single_thread)
 {
-    MailBox<int, 10> mail_box;
+    for (std::size_t buffer_size = 1000; buffer_size >= 10; buffer_size /= 10) {
+        MailBox<int, 100> mail_box;
 
-    SingleWriterRingBuffer buffer(100);
+        SingleWriterRingBuffer<int> buffer(buffer_size);
 
-    for (int i = 0; i < 10; ++i)
-        buffer.emplace_front(i);
+        for (int i = 0; i < 100; ++i)
+            buffer.emplace_front(i);
 
+        for (int i = 0; i < 100; ++i) {
+            ASSERT_TRUE(try_pop_back(next)) << "buffer under-filled";
 
-    for (int i = 0; i < 10; ++i) {
-        ASSERT_TRUE(try_pop_back(next)) << "buffer under-filled";
+            mail_box.push_back(next);
+        }
 
-        mail_box.push_back(next);
+        ASSERT_FALSE(try_pop_back(next)) << "buffer over-filled";
+
+        MailBox<int, 100>::const_iterator mail_box_iter = mail_box.cbegin();
+
+        for (int i = 0; i < 100; ++mail_box_iter, ++i)
+            ASSERT_EQ(i, *mail_box_iter) << "insertions out of order";
+
+        ASSERT_EQ(mail_box.cend(), mail_box_iter) << "mail_box over-filled";
     }
-
-    ASSERT_FALSE(try_pop_back(next)) << "buffer over-filled";
-
-    MailBox<int, 10>::const_iterator mail_box_iter = mail_box.cbegin();
-
-    for (int i = 0; i < 10; ++mail_box_iter, ++i)
-        ASSERT_EQ(i, *mail_box_iter) << "insertions out of order";
-
-    ASSERT_EQ(mail_box.cend(), mail_box_iter) << "mail_box over-filled";
 }
